42491f21c2dc3ba155717386f425674cfe521203
Dennis Lee <dennislwm@yahoo.com.au>
2024-01-20 14:35:02 +0800

    Initial commit get_sub

---
+++ Makefile
@@ -0,0 +1,6 @@
+.PHONY: sub
+SHELL := /opt/homebrew/bin/bash
+
+sub:
+	@test $(URL) || ( echo [Usage] make sub URL=YOUTUBE_LINK; exit 1 )
+	@source ./yt-dlp.sh && get_sub $(URL)
---
+++ yt-dlp.sh
@@ -0,0 +1,14 @@
+function get_sub {
+  if [ -z "$1" ]; then
+    echo "[Usage][$FUNCNAME]: $FUNCNAME URL"
+    return 1
+  fi
+  yt-dlp --version > /dev/null 2>&1 || ( echo "[ERROR][$FUNCNAME]: yt-dlp not installed." && return 1 )
+  jq --version > /dev/null 2>&1 || ( echo "[ERROR][$FUNCNAME]: jq not installed." && return 1 )
+  local url="$1"
+  FILE=$( yt-dlp -e "${url}" )
+  echo $FILE.txt
+  yt-dlp --skip-download --write-auto-sub --quiet --output "${FILE}" --sub-format json3 ${URL}
+  jq -r '.events[]|select(.segs and.segs[0].utf8!="\n")|[.segs[].utf8]|join("")' "${FILE}.en.json3" | paste -sd\  -|fold -sw60yt-dlp >> "${FILE}.txt"
+  rm "${FILE}.en.json3"
+}
